630a98fcc6c2f677ea8acd33de6272784710d564
Antoniojvr1 <antoniovasquezrojas528@gmail.com>
2025-04-06 12:06:26 -0600

    Primer commit

---
+++ FrontEndTiendaOnline/Models/Pago.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEndTiendaOnline.Models
+{
+    public class Pago
+    {
+        private int _id;
+        private Pedido _pedido;
+        private MetodoPago _metodoPago;
+        private DateTime _fechaPago;
+
+        public Pago()
+        {
+
+        }
+
+        public Pago(int id, DateTime fechaPago, Pedido pedido, MetodoPago metodoPago)
+        {
+            Id = id;
+            MetodoPago = metodoPago;
+            FechaPago = fechaPago;
+            Pedido = pedido;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public MetodoPago MetodoPago { get => _metodoPago; set => _metodoPago = value; }
+        public DateTime FechaPago { get => _fechaPago; set => _fechaPago = value; }
+        public Pedido Pedido { get => _pedido; set => _pedido = value; }
+    }
+}
